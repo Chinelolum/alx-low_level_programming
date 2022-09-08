ec6737949c6c825bfb08f6740b3388ef8bdbcabe
Chinelolum <arinzeannette@gmail.com>
2022-09-08 10:53:28 -0700

    how far work abeg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- *
- * Return : 0 after printing the function
- */
+ * * main - Printing
+ * *
+ * * Return: Always 0 (Successful)
+ * */
 int main(void)
 {
-	puts ("\"programming is like building a multilingual puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual
+			puzzle");
+	return (0);
 }
