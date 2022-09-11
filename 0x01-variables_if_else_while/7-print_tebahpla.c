d96fed04a0c70789b12db51a4858274578909f34
Chinelolum <arinzeannette@gmail.com>
2022-09-11 14:57:35 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0 to 9
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (c = 'z'; n <= 'a'; c--)
+	{
+		putchar(c);
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
