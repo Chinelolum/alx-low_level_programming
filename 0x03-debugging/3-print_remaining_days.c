e5cb8998367b8c48398880e8f4b727363f380e2b
Chinelolum <arinzeannette@gmail.com>
2022-09-16 09:56:15 -0700

    newremaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,8 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		printf("Day of the year: %d\n", 366 - day);
+
+		printf("Day of the year: %d\n", 365 - day);
 	}
 	else
 	{
