9dea6f80d50bbb63f892a8e1e54466ddbab3fbeb
Chinelolum <arinzeannette@gmail.com>
2022-09-16 10:02:10 -0700

    newnew

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-void print_remaiining_days(int, int, int);
+void print_remaining_days.c(int, int, int);
 int convert_day(int, int);
+int like_comment_subscribe(void)
 
 #endif /* MAIN_H */
