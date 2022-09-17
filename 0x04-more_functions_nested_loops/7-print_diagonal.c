7e1219c675de434c719bb80502268a981fce5a83
Chinelolum <arinzeannette@gmail.com>
2022-09-16 19:00:37 -0700

    Finesse

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_diagonal - draws diagonal line
+ * @n: number og\f lines
+ *
+ * Return : empty
+ */
+
+void print_diagonal(int n)
+
+{
+	int line = 0;
+	int spaces;
+
+	if (n > 0)
+
+	{
+
+		while (line < n)
+
+		{
+
+			for (spaces = 0; spaces < line; spaces++)
+			{
+				_putchar (' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			line++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
