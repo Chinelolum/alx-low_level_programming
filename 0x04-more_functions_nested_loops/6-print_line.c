f1df3d24da5766811bbcd1b77ad5ae85651c2568
Chinelolum <arinzeannette@gmail.com>
2022-09-16 18:51:34 -0700

    sungba

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print a straight line
+ * @n: printed line
+ */
+
+void print_line(int n)
+{
+
+	int x = 0;
+
+	while (x < n)
+	{
+		_putchar('_');
+	x++;
+	}
+	_putchar('\n');
+}
