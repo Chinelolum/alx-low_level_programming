1956666ba6a66092b59006ae807e164734714bd5
Chinelolum <arinzeannette@gmail.com>
2022-09-16 17:39:32 -0700

    monalisa

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - print normal numbers
- *
- * Return : empty
+ * print_numbers - print numbers 0 to 9
  */
+
 void print_number(void)
 {
-	int c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+	for (i = '0'; i  <= '9'; i++)
+		_putchar(i);
+			_putchar('\n');
 }
