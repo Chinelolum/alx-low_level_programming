c1637efda67f08cb9aef6d235b31a11f52e674da
Chinelolum <arinzeannette@gmail.com>
2022-09-22 03:00:52 -0700

    stringcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * _strncat - adding two strings
- * @dest: return a point to the resulting string
+ * @dest: initalized
  * @src: variables
  * @n: another variable
- * Return: a character
+ * Return: returns a character
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0;
 	int b = 0;
+
 
 
 	while (dest[a] != '\0')
